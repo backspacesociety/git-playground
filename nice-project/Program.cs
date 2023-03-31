8750f72dad73d8ed3d491091e7d302838b759540
backspacesociety <goloburda.ivan@lll.kpi.ua>
2023-03-31 16:00:58 +0300

    Removed merge conflict text

--- nice-project/Program.cs
+++ nice-project/Program.cs
@@ -7,12 +7,7 @@ namespace NiceProject
         static void Main(string[] args)
         {
             Console.WriteLine("I'm doing some very important stuff!");
-
-<<<<<<< HEAD
-            var fileReader = new TextFileReader("../../../input.txt");
-=======
             var fileReader = new EmptyFileReader();
->>>>>>> another-merge-conflict-branch
             
             var stuff = new ImportantStuff();
             var result = stuff.DoWhatNeedsToBeDone(fileReader);
